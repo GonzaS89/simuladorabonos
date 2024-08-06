fbd7711ad03047b8401fe39524d4e27275969f91
Gonza <gnzx89@gmail.com>
2024-08-06 15:17:47 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import "./App.css";
-import React, { useState, useEffect } from 'react';
-import ListaLocalidadesInicio from "./Componentes/ListaLocalidadesInicio";
-import ListaLocalidadesDestino from "./Componentes/ListaLocalidadesDestino";
-import InicioDestino from "./Componentes/InicioDestino";
 
 function App () {
 
-  const [localidad , setLocalidad] = useState(null)
-
-  const info = (data) => {
-    setLocalidad(data)
-  }
-
-  useEffect(() => {
-    console.log(localidad)
-  },[localidad])
-
   return (
     <div className="App">
-      <InicioDestino inicio = {localidad}/>
-      <div className="contenedor-general">
-        <div className="contenedor-slide-localidades">
-          <ListaLocalidadesInicio localidadClickeada={info}/>
-          <ListaLocalidadesDestino />
-        </div>
-      </div>
+      
     </div>
   );
 }
