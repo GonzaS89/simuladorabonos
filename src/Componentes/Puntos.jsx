7e9763141f2c6b5e3698ce7bdeca6380c73619a7
Gonza <gnzx89@gmail.com>
2024-08-08 12:54:11 -0300

    trabajando...

--- src/Componentes/Puntos.jsx
+++ src/Componentes/Puntos.jsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 import '../Estilos/salidallegada.css';
 
 
-export const Puntos = ( { listaDeLocalidadesDesplegada , nombre , puntoElegido , localidadOrigen , localidadDestino })  => {
+export const Puntos = ( { listaDeLocalidadesDesplegada , nombre , puntoElegido , localidadOrigen , localidadDestino , clickeoDeCampo})  => {
 
     const [textoCampoLocalidad , setTextoCampoLocalidad] = useState('Elegí una localidad');
-
     const [origenOdestino , setOrigenOdestino] = useState();
 
     const clickeadoEnMenuDeLocalidades = () => {
         return (
+                setTextoCampoLocalidad('Elegí una localidad'),
                 listaDeLocalidadesDesplegada(true),
                 puntoElegido(nombre),
                 setOrigenOdestino(nombre)
@@ -17,9 +17,10 @@ export const Puntos = ( { listaDeLocalidadesDesplegada , nombre , puntoElegido ,
     }
 
     useEffect(() => {
+        
         origenOdestino === 'origen' && setTextoCampoLocalidad(localidadOrigen);
         origenOdestino === 'destino' && setTextoCampoLocalidad(localidadDestino);
-    },[localidadOrigen , localidadDestino , origenOdestino])
+    },[localidadOrigen , localidadDestino , origenOdestino , campoClickeado])
 
     return (
         <div className="puntos"> 
