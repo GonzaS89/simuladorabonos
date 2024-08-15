836c06e0772e9fd48b33c60b8aac67d96bc9ee1c
Gonza <gnzx89@gmail.com>
2024-08-15 15:44:21 -0300

    creando screen horarios

---
+++ src/ComponentesHorarios/IdaVuelta.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Estilos/horarios.css';
+import { FaArrowRight } from "react-icons/fa";
+
+
+const IdaVuelta = ( { nombre }) => {
+    return (
+        <div className='container-idavuelta'>
+            <div className='texto-boton-idavuelta'><p>{nombre}</p></div>
+            <FaArrowRight className='icono-flecha'/>
+
+        </div>
+    )
+}
+
+export default IdaVuelta;
--- src/Screens/Horarios.jsx
+++ src/Screens/Horarios.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
 import '../Estilos/screens.css'
 import '../Estilos/horarios.css'
+import Grilla from '../grillas.json';
 import { useEffect, useState } from 'react';
+import IdaVuelta from '../ComponentesHorarios/IdaVuelta';
 
-const Horarios = ( ) => {
+const Horarios = () => {
 
-   
-    const [minutes, setMinutes] = useState(new Date().getMinutes());
 
-    useEffect(() => {
-      const updateMinutes = () => {
-        setMinutes(new Date().getMinutes());
-      };
-  
-      // Actualiza cada minuto
-      const timerIdMinutes = setInterval(updateMinutes, 1000);
-  
-      // También actualiza inmediatamente cuando el componente se monta
-      updateMinutes();
-  
-      // Limpia el intervalo cuando el componente se desmonte
-      return () => clearInterval(timerIdMinutes);
-    }, []);
-    
+  const [minutes, setMinutes] = useState(new Date().getMinutes());
 
-    return (
-        <div className='container-screen'>
-            <div className='container-main-horarios'></div>
+  useEffect(() => {
+    console.log(Grilla.lunesAViernes.idas['floridaxalderetes/alternativa'])
+    const updateMinutes = () => {
+      setMinutes(new Date().getMinutes());
+    };
+
+    // Actualiza cada minuto
+    const timerIdMinutes = setInterval(updateMinutes, 1000);
+
+    // También actualiza inmediatamente cuando el componente se monta
+    updateMinutes();
+
+    // Limpia el intervalo cuando el componente se desmonte
+    return () => clearInterval(timerIdMinutes);
+  }, []);
+
+
+  return (
+    <div className='container-screen'>
+      <div className='container-main-horarios'>
+        <div className='container-ida-vuelta'>
+          <IdaVuelta nombre = {'ida'}/>
+          <IdaVuelta nombre = {'vuelta'}/>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Horarios;
