06ae6f3e62bb5e53f33022501634bbe0d2a1c6ea
GonzaS89 <gnzx89@gmail.com>
2024-09-10 00:50:02 -0300

    definiendo algoritmo de abonos

--- src/ComponentesAbonos/OpcionLocalidad.jsx
+++ src/ComponentesAbonos/OpcionLocalidad.jsx
@@ -1,31 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import '../Estilos/opcionsalidadestino.css';
+import React, { useEffect, useState } from "react";
+import "../Estilos/opcionsalidadestino.css";
 import { FaCheckCircle } from "react-icons/fa";
 
-
-
-export const OpcionLocalidad = ({ nombre , enviarLocalidad, localidadOrigen }) => {
-
+export const OpcionLocalidad = ({
+  nombre,
+  enviarLocalidad,
+  localidadOrigen,
+}) => {
   const [localidadClickeada, setLocalidadClickeada] = useState(false);
 
-
   const clickearImg = () => {
-    enviarLocalidad(nombre)
-  }  
-  useEffect(() => {
-    localidadOrigen === nombre ? setLocalidadClickeada(true) : setLocalidadClickeada(false)
-  },[localidadOrigen, nombre])
- 
-  
+    enviarLocalidad(nombre);
+  };
 
+  useEffect(() => {
+    localidadOrigen === nombre
+      ? setLocalidadClickeada(true)
+      : setLocalidadClickeada(false);
+  }, [localidadOrigen, nombre]);
 
   return (
     <div className="container-salida-localidad" onClick={clickearImg}>
       <div className="imagen-localidad-opcion">
-      <img src={require(`../IMG/${nombre}.avif`)} alt=""className={localidadClickeada ? 'sombrearImg' : ''}/>
-      <FaCheckCircle className={localidadClickeada ? 'icono-checked mostrarIconoCheked' : 'icono-checked hidden'}/>
+        <img
+          src={require(`../IMG/${nombre}.avif`)}
+          alt=""
+          className={localidadClickeada ? "sombrearImg" : ""}
+        />
+        <FaCheckCircle
+          className={
+            localidadClickeada
+              ? "icono-checked mostrarIconoCheked"
+              : "icono-checked hidden"
+          }
+        />
       </div>
-        <p>{nombre}</p>
+      <p>{nombre}</p>
     </div>
-  )
-  }
+  );
+};
---
+++ src/ComponentesHorarios/ContainerHoraDia.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Estilos/horarios.css';
+
+export const ContainerHoraDia = () => {
+  return (
+    <div className="container-bloque_hora-dia">
+        <div className="container-hora">
+            div.
+        </div>
+    </div>
+  )
+}
